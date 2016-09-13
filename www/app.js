31b1bfc8445834e4de295ad901600640fe56e46a
Alex Abbott <alex.abbott@bynd.com>
2016-09-13 14:12:53 -0400

    Fix JS lint warnings

--- www/app.js
+++ www/app.js
@@ -1,6 +1,7 @@
 angular.module('contentApp', ['ngSanitize', 'angular-sortable-view'])
 
 .config(['$locationProvider', function($locationProvider) {
+  'use strict';
   $locationProvider.html5Mode({
     enabled: true,
     requireBase: false
@@ -8,68 +9,72 @@ angular.module('contentApp', ['ngSanitize', 'angular-sortable-view'])
 }])
 
 .run(['$location', function($location) {
+  'use strict';
   if ($location.path() != '/custom.html') {
     $location.path('');
   }
 }])
 
 .factory('dataObject', function(){
+  'use strict';
   var content = {
-    "sections": [
+    sections: [
       {
-        "section": "General",
-        "subsections": [
+        section: "General",
+        subsections: [
           {
-            "subsection": "Typography",
-            "content": "some typography content<br><iframe src='https://docs.google.com/presentation/d/1yTyEpqX3--enNQs5xWxl25ebEbmyrQnpsAmOlDahJZo/embed?start=false&loop=false&delayms=3000' frameborder='0' allowfullscreen='true' mozallowfullscreen='true' webkitallowfullscreen='true'></iframe>"
+            subsection: "Typography",
+            content: "some typography content<br><iframe src='https://docs.google.com/presentation/d/1yTyEpqX3--enNQs5xWxl25ebEbmyrQnpsAmOlDahJZo/embed?start=false&loop=false&delayms=3000' frameborder='0' allowfullscreen='true' mozallowfullscreen='true' webkitallowfullscreen='true'></iframe>"
           },
           {
-            "subsection": "Colors",
-            "content": "<p>Lots of text about some things and some other things</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>"
+            subsection: "Colors",
+            content: "<p>Lots of text about some things and some other things</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p><p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>"
           }
         ]
       },
       {
-        "section": "Advertising",
-        "subsections": [
+        section: "Advertising",
+        subsections: [
           {
-            "subsection": "Digital",
-            "content": "some digital ad content<br><iframe data-src='//docs.google.com/gview?url=https://storage.googleapis.com/doubleclick-prod/documents/Programmatic_Direct_Infographic.pdf&amp;embedded=true' allowfullscreen='' src='//docs.google.com/gview?url=https://storage.googleapis.com/doubleclick-prod/documents/Programmatic_Direct_Infographic.pdf&amp;embedded=true'></iframe>"
+            subsection: "Digital",
+            content: "some digital ad content<br><iframe data-src='//docs.google.com/gview?url=https://storage.googleapis.com/doubleclick-prod/documents/Programmatic_Direct_Infographic.pdf&amp;embedded=true' allowfullscreen='' src='//docs.google.com/gview?url=https://storage.googleapis.com/doubleclick-prod/documents/Programmatic_Direct_Infographic.pdf&amp;embedded=true'></iframe>"
           },
           {
-            "subsection": "Print",
-            "content": "some print ad content<br><img src='https://storage.googleapis.com/doubleclick-prod/images/offset_comp_237030_1.95b375d5.rectangle-550x462.jpg'>"
+            subsection: "Print",
+            content: "some print ad content<br><img src='https://storage.googleapis.com/doubleclick-prod/images/offset_comp_237030_1.95b375d5.rectangle-550x462.jpg'>"
           },
           {
-            "subsection": "Email",
-            "content": "some email ad content<ul><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li></ul><strong>Bold text is cool</strong>"
+            subsection: "Email",
+            content: "some email ad content<ul><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li></ul><strong>Bold text is cool</strong>"
           }
         ]
       },
       {
-        "section": "Sports",
-        "subsections": [
+        section: "Sports",
+        subsections: [
           {
-            "subsection": "Baseball",
-            "content": "some baseball content<br><iframe src='https://www.youtube.com/embed/6Joup252fR0' frameborder='0' allowfullscreen></iframe>"
+            subsection: "Baseball",
+            content: "some baseball content<br><iframe src='https://www.youtube.com/embed/6Joup252fR0' frameborder='0' allowfullscreen></iframe>"
           },
           {
-            "subsection": "Basketball",
-            "content": "some basketball content<br><iframe src='https://www.youtube.com/embed/O1uxFRIdCWo' frameborder='0' allowfullscreen></iframe>"
+            subsection: "Basketball",
+            content: "some basketball content<br><iframe src='https://www.youtube.com/embed/O1uxFRIdCWo' frameborder='0' allowfullscreen></iframe>"
           },
           {
-            "subsection": "Soccer",
-            "content": "some soccer content<br><iframe src='https://www.youtube.com/embed/mI2dDwWaNUY' frameborder='0' allowfullscreen></iframe>"
+            subsection: "Soccer",
+            content: "some soccer content<br><iframe src='https://www.youtube.com/embed/mI2dDwWaNUY' frameborder='0' allowfullscreen></iframe>"
           }
         ]
       }
     ]
-  }
+  };
 
   return content;
 })
 
 .controller('mainController', ['$scope', '$timeout', '$location', '$sce', 'dataObject', function($scope, $timeout, $location, $sce, dataObject) {
+  'use strict';
+
   $scope.content = dataObject;
   $scope.primaryContent = [];
   $scope.custom = [];
@@ -89,7 +94,7 @@ angular.module('contentApp', ['ngSanitize', 'angular-sortable-view'])
 
   // show the parent section in the main content area and scroll down to subsection anchor
   $scope.showSubsection = function() {
-    var me = this
+    var me = this;
     $scope.primaryContent = this;
     $timeout(function(){
       location.hash = "#" + me.sub.subsection.toLowerCase();
@@ -125,16 +130,11 @@ angular.module('contentApp', ['ngSanitize', 'angular-sortable-view'])
     $scope.custom.splice(index, 1);
   };
 
-  // TODO copy custom link
-  $scope.copyLink = function (containerid) {
-    var range = document.createRange();
-    range.selectNode(document.getElementById(containerid));
-    window.getSelection().addRange(range);
-  };
-
 }])
 
 .controller('customController', ['$scope', '$location', '$sce', 'dataObject', function($scope, $location, $sce, dataObject) {
+  'use strict';
+
   $scope.content = dataObject;
   var subsections = [];
   var customContent = [];
