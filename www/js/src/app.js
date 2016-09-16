55e002d5d771bc4c7b217ad1a9a8083ebcd94c2b
Alex Abbott <alex.abbott@bynd.com>
2016-09-16 16:43:13 -0400

    Update styling

--- www/js/src/app.js
+++ www/js/src/app.js
@@ -26,32 +26,32 @@ angular.module('contentApp', ['ngSanitize', 'angular-sortable-view', 'smoothScro
           {
             subsection: "Android Mobile Solutions",
             content: "<div class='card-slide'><h3 class='headline'>Android Mobile Solutions Power Next Generation Business Transformation:</h3><div class='card-holder'><div class='card small'><i class='material-icons'>group</i><p>Improve efficiency and collaboration by <strong>completely connecting kand untethering</strong> your dispersed workforce.</p></div><div class='card small'><i class='material-icons'>trending_up</i><p>Increase confidence, control, and cost savings by <strong>unifying security and management</strong> for every device and every screen.</p></div><div class='card small'><i class='material-icons'>device_hub</i><p>Enable fast, proactive, and strategic decision making by <strong>digitizing processes</strong> and <strong>connecting to data and insights.</strong></p></div></div></div>",
-            notes: "Android mobile solutions is lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            notes: "Talk about the value behind Google’s vision and what can be achieved through mobile solutions. Describe what impact looks like specifically. 'Mobility can transform your business, bringing long-term value across the enterprise.' Highlight Android and Google willingness to listen and understand current business pressures."
           },
           {
             subsection: "Worlds Biggest OS",
             content: "<div class='stat-slide'><h3 class='headline'>Android, the World's Biggest OS</h3><div class='card-holder'><div class='card small'><div class='stat'>81%</div><p>of the world's mobile devices</p></div><div class='card small'><div class='stat'>500+</div><p>operator partnerships</p></div><div class='card small'><div class='stat'>1.5M</div><p>Android devices activated daily</p></div><div class='card small'><div class='stat'>1.4B+</div><p>30-day active users</p></div><div class='card small'><div class='stat'>400+</div><p>OEMs developing for Android</p></div><div class='card small'><div class='stat'>45%</div><p>share of U.S. corporate purchased smartphones</p></div></div></div>",
-            notes: "World's biggest OS by lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+            notes: "Highlight that Android has strong partners and is the OS of choice in the consumer and corporate space. 'With Android, you’ll be building with a solid network of partners and a high volume of adoption in both the consumer and corporate space.' 'Start with the world’s biggest, open-source operating system—the OS that your global workforce is using and familiar with today.'"
           }
         ]
       },
       {
-        section: "Advertising",
+        section: "Collective Intelligence",
         subsections: [
           {
-            subsection: "Digital",
-            content: "some digital ad content<br><iframe data-src='//docs.google.com/gview?url=https://storage.googleapis.com/doubleclick-prod/documents/Programmatic_Direct_Infographic.pdf&amp;embedded=true' allowfullscreen='' src='//docs.google.com/gview?url=https://storage.googleapis.com/doubleclick-prod/documents/Programmatic_Direct_Infographic.pdf&amp;embedded=true'></iframe>",
-            notes: "Digital Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            subsection: "Business Transformation",
+            content: "<div class='card-slide'><h3 class='headline'>Android Mobile Solutions Power Next Generation Business Transformation:</h3><div class='card-holder'><div class='card small'><i class='material-icons'>face</i><p>Elevate service and increase revenue by <strong>enabling a 360 degree, cross-channel view of your customers.</strong></p></div><div class='card small'><i class='material-icons'>domain</i><p>Improve employee and customer experience by <strong>creating a connected and intuitive workplace.</strong></p></div><div class='card small'><i class='material-icons'>star_border</i><p>Attract talent and empower your workforce by <strong>providing the smartest, most efficient tools for any job, anywhere.</strong></strong></p></div></div></div>",
+            notes: "Continue to talk about the transformational value behind Google’s vision and what can be achieved through mobile solutions."
           },
           {
-            subsection: "Print",
-            content: "some print ad content<br><img src='https://storage.googleapis.com/doubleclick-prod/images/offset_comp_237030_1.95b375d5.rectangle-550x462.jpg'>",
-            notes: "Print Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            subsection: "Google Play",
+            content: "<div class='stat-slide three-stats'><h3 class='headline'>Google Play, the Largest Distribution Platform Available:</h3><div class='card-holder'><div class='card small'><div class='stat'>65B+</div><p>App installs through Play</p></div><div class='card small'><div class='stat'>1M+</div><p>Apps available</p></div><div class='card small'><div class='stat'>1B+</div><p>30-day active users of Google Apps</p></div></div></div>",
+            notes: "Continue that you also have access to the largest secure distribution platform. 'And build with the largest distribution platform available with Google Play.'"
           },
           {
-            subsection: "Email",
-            content: "some email ad content<ul><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li><li>Bullet points</li></ul><strong>Bold text is cool</strong>",
-            notes: "Email Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            subsection: "New in Android N",
+            content: "<div class='video-slide'><iframe src='https://www.youtube.com/embed/CsulIu3UaUM' frameborder='0' allowfullscreen></iframe></div>",
+            notes: "New in Android N and lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           }
         ]
       },
@@ -59,19 +59,19 @@ angular.module('contentApp', ['ngSanitize', 'angular-sortable-view', 'smoothScro
         section: "Security",
         subsections: [
           {
-            subsection: "New in Android N",
-            content: "<div class='video-slide'><iframe src='https://www.youtube.com/embed/CsulIu3UaUM' frameborder='0' allowfullscreen></iframe></div>",
-            notes: "New in Android N and lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          },
-          {
             subsection: "Continued Investment",
             content: "<div class='card-image-slide'><div class='left'><h3 class='headline'>Continued Google Investment in Security Programs</h3><div class='card large'><h4 class='subheadline'>Security Reward Program:</h4><ul class='arrow-points'><li>Reports from 35 researchers have helped make Android security stronger.</li><li>More than one third or reports were made in Media Server, which has been hardened to resist vulnerabilities.</li></ul><h4 class='subheadline'>OEM Programs:</h4><ul class='arrow-points'><li>CTS Test</li><li>CDD Compatibility</li></ul></div></div><div class='right'><img src='img/phone-security.png' alt='security'></div></div>",
-            notes: "Security investment of lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            notes: "Outline specific security programs that Google supports, putting the security researcher community to work in Security Rewards Program, and the CTS Test and CDD Compatibility in partnership with OEMS."
           },
           {
             subsection: "Continual Updates",
             content: "<div class='card-image-slide'><div class='left'><h3 class='headline'>Continual Updates</h3><div class='card large'><h4 class='subheadline'>SafetyNet:</h4><ul class='arrow-points'><li>400 million device scans a day</li><li>6 billion apps checked per day</li></ul></div></div><div class='right'><img src='img/phone-updates.png' alt='continual updates'></div></div>",
-            notes: "Continual updates in lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            notes: "Explain in additional detail how Android provides continual updates and verifications; tie to Google security services, what they enable and ensure, and how Google intelligence and machine learning supports Android platform security. 'Google Play Services, Application Analysis and Safety Net all work in the cloud to ensure that at any time you download a new app on your device, you are protected from potentially harmful apps, data corruption and data leakage.'"
+          },
+          {
+            subsection: "Android Fingerprints",
+            content: "<div class='video-slide'><iframe src='https://www.youtube.com/embed/5HbDQQ5WlS8' frameborder='0' allowfullscreen></iframe></div>",
+            notes: "Android fingerprints and lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           }
         ]
       }
@@ -314,8 +314,12 @@ angular.module('contentApp', ['ngSanitize', 'angular-sortable-view', 'smoothScro
       $scope.fullscreenCounter += 1;
       if ($scope.fullscreenCounter % 2 === 0) {
         angular.element(document.querySelector('.fullscreen')).removeClass('ng-hide');
+        angular.element(document.querySelector('.controls')).removeClass('hide');
+        angular.element(document.querySelector('.slide-holder')).removeClass('full');
       } else {
         angular.element(document.querySelector('.fullscreen')).addClass('ng-hide');
+        angular.element(document.querySelector('.controls')).addClass('hide');
+        angular.element(document.querySelector('.slide-holder')).addClass('full');
       }
     }
   }
